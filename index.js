960bc9051f247565880d4726845d93bab1207bd7
Leandro <c.s.leandro@live.com>
2023-04-12 14:08:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const produtos = require('./produtos')
+
+const app = express()
+const port = 443
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+const public = path.join(__dirname, 'public')
+app.use(express.static(public));
+
+app.get('/', (_, res) => res.render('home'))
+app.use('/produtos', produtos)
+app.get('/contato', (_, res) => res.render('contato'))
+app.get('/sobre', (_, res) => res.render('sobre'))
+
+app.listen(port, () => console.log('Server running on port', port))
