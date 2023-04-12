2112390ae9121f24d654a0c71c77c11d5a24914b
Leandro <c.s.leandro@live.com>
2023-04-12 15:17:18 -0300

    max-width na exibição de produtos.. responsividade

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const path = require('path')
-const produtos = require('./produtos')
+const express = require("express");
+const path = require("path");
+const produtos = require("./produtos");
 
-const app = express()
-const port = 443
+const app = express();
+const port = 443;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-const public = path.join(__dirname, 'public')
+const public = path.join(__dirname, "public");
 app.use(express.static(public));
 
-app.get('/', (_, res) => res.render('home'))
-app.use('/produtos', produtos)
-app.get('/contato', (_, res) => res.render('contato'))
-app.get('/sobre', (_, res) => res.render('sobre'))
+app.get("/", (_, res) => res.render("home"));
+app.use("/produtos", produtos);
+app.get("/contato", (_, res) => res.render("contato"));
+app.get("/sobre", (_, res) => res.render("sobre"));
 
-app.listen(port, () => console.log('Server running on port', port))
+app.listen(port, () => console.log("Server running on port", port));
--- produtos.js
+++ produtos.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-const router = express.Router()
-router.get('/', (_, res) => {
-  const db = JSON.parse(fs.readFileSync('public/db.json', 'utf-8'))
-  res.render('produtos', { produtos: db.produtos })
-})
+const router = express.Router();
+router.get("/", (_, res) => {
+  const db = JSON.parse(fs.readFileSync("public/db.json", "utf-8"));
+  res.render("produtos", { produtos: db.produtos });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const { id } = req.params;
-  const db = JSON.parse(fs.readFileSync('public/db.json', 'utf-8'))
-  const produto = db.produtos.find(p => p.id === Number.parseInt(id))
-  res.render('produto', { produto })
-})
+  const db = JSON.parse(fs.readFileSync("public/db.json", "utf-8"));
+  const produto = db.produtos.find((p) => p.id === Number.parseInt(id));
+  res.render("produto", { produto });
+});
 
-module.exports = router
+module.exports = router;
